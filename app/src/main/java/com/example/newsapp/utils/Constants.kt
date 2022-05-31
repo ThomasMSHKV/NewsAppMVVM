5d91962244ff16dd598c501c9558d56d03c963ba
OlegDeveloper <tmereshkov@gmail.com>
2022-05-31 20:27:32 +0300

    add retrofit2 library

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object {
+        const val API_KEY = "6affd06c5b6f4dbd84ae7a267ce72821"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
