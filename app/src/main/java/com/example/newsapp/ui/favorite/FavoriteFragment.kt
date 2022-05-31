51391ba133a248b7c9f36eefe5685f1dbc751b8d
OlegDeveloper <tmereshkov@gmail.com>
2022-05-31 13:51:07 +0300

    add navigationFragment Library

---
+++ app/src/main/java/com/example/newsapp/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,23 @@
+package com.example.newsapp.ui.favorite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.newsapp.databinding.FragmentFavoriteBinding
+
+
+class FavoriteFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteBinding? = null
+    private val mBinding get() =  _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFavoriteBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+}
